85a42bdfc219df5db4b9ed4037f2a0e3055618ac
kuangyi <xingkuangyi@gmail.com>
2025-04-27 19:27:47 +0200

    update the layout

--- src/@lekoarts/gatsby-theme-cara/components/projects.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/projects.tsx
@@ -12,7 +12,7 @@ const Projects = () => (
       "@media only screen and (min-width: 640px)": {
         display: `grid`,
         gridGap: `50px`,
-        gridTemplateColumns: `repeat(3, 1fr)`,
+        gridTemplateColumns: `repeat(4, 1fr)`,
         h2: { gridColumn: `-1/1` },
         gridAutoRows: `minmax(300px, auto)`,
       },
