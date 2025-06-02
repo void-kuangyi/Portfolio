0dedf32f938634cb6483a5b6b79903e15767f18f
kuangyi <xingkuangyi@gmail.com>
2025-06-02 14:21:05 +0200

    add pcb and ai kit

---
+++ src/pages/ai-kit.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import AiKitContent from "../@lekoarts/gatsby-theme-cara/sections/ai-kit";
+import ContentLayout from "../@lekoarts/gatsby-theme-cara/components/content-layout";
+
+const AiKit = () => (
+  <ContentLayout>
+    <AiKitContent />
+  </ContentLayout>
+);
+
+export default AiKit;
---
+++ src/pages/lumus.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import LumusContent from "../@lekoarts/gatsby-theme-cara/sections/lumus";
+import ContentLayout from "../@lekoarts/gatsby-theme-cara/components/content-layout";
+
+const Lumus = () => (
+  <ContentLayout>
+    <LumusContent />
+  </ContentLayout>
+);
+
+export default Lumus;
