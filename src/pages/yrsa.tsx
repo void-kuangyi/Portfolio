961fee788a690912e5eb977c7b7fb1c2876b0b75
kuangyi <xingkuangyi@gmail.com>
2025-08-28 15:04:47 +0200

    add yrsa
    update the text
    
    make file smaller
    
    reduce the file size

---
+++ src/pages/yrsa.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout";
+import Inner from "../@lekoarts/gatsby-theme-cara/elements/inner";
+import YrsaContent from "../@lekoarts/gatsby-theme-cara/sections/yrsa";
+import ContentLayout from "../@lekoarts/gatsby-theme-cara/components/content-layout";
+
+const Yrsa = () => (
+  <ContentLayout>
+    <YrsaContent />
+  </ContentLayout>
+);
+
+export default Yrsa;
